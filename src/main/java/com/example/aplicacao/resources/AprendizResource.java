6e589834312ce7d730c273ab11b466c9152d7d97
Henrique de Castro <lhccorrea@hotmail.com>
2018-12-13 13:42:31 -0200

    Paginacao com parametros Aprendiz

--- src/main/java/com/example/aplicacao/resources/AprendizResource.java
+++ src/main/java/com/example/aplicacao/resources/AprendizResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,6 +42,17 @@ public class AprendizResource {
 	public ResponseEntity<List<AprendizDTO>> getAll() {
 		List<Aprendiz> lista = servico.findAll();
 		List<AprendizDTO> listaDTO = lista.stream().map(obj -> new AprendizDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+	@RequestMapping(value = "/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<AprendizDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0")Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		Page<Aprendiz> lista = servico.findPage(page, linesPerPage, orderBy, direction);
+		Page<AprendizDTO> listaDTO = lista.map(obj -> new AprendizDTO(obj));
 		return ResponseEntity.ok().body(listaDTO);
 	}
 
--- src/main/java/com/example/aplicacao/servico/AprendizService.java
+++ src/main/java/com/example/aplicacao/servico/AprendizService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.aplicacao.dominio.Aprendiz;
@@ -53,5 +56,10 @@ public class AprendizService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos cadastrados");
 		};
 		
+	}
+	
+	public Page<Aprendiz> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
